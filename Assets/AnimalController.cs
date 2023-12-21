b97f20959060d6785f45bdd31a0f880dfd75fc85
Sudhanshu Kr. Suman <unknown.what.443@gmail.com>
2023-12-21 12:05:08 +0530

    updated AnimalController script
    tried to used all pillars of OOPs

--- Assets/AnimalController.cs
+++ Assets/AnimalController.cs
@@ -2,21 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimalController : MonoBehaviour
-{
-    private Rigidbody rb;
-    private float force = 5f;
 
+//ENCAPSULATION
+public class Animal : MonoBehaviour
+{
+    //ABSTRACTION
+    protected Rigidbody Rb { get; set; }
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        Rb = GetComponent<Rigidbody>();
     }
+    public virtual void Jump(float force)
+    {
+        Rb.AddForce(Vector3.up * force);
+    }
+}
+
+//INHERITANCE
+public class AnimalController : Animal
+{
+    private float force = 5f;
 
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+            Jump(force);
         }
+    }
+
+    //POLYMORPHISM
+    public override void Jump(float force)
+    {
+        Rb.AddForce(Vector3.up * force, ForceMode.Impulse);
     }
 }
